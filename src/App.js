7fdfc907ff09737824d9fdc0305ed8ecfddd8123
Brian Shapiro <briandavidshapiro@gmail.com>
2021-04-24 22:05:44 -0400

    responsive grid working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 // import Accordion from "./components/Accordion";
 // import Form from "./components/Form";
-// import Grid from "./components/Grid";
+import Grid from "./components/Grid/Grid";
+import GridItem from "./components/Grid/GridItem";
 // import Message from "./components/Message";
 // import Modal from "./components/Modal";
 // import ProgressBar from "./components/ProgressBar";
@@ -15,24 +15,32 @@ import CardImage from "./components/Card/CardImage";
 import CardActions from "./components/Card/CardActions";
 import Avatar from "./components/Avatar";
 
+const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 function App() {
 	return (
 		<div className="App">
-			<Card bg="dark">
-				<CardHeader title="Title" subtitle="Subtitle" avatarContent="R" />
-				<CardImage
-					image="https://images.unsplash.com/photo-1614822282964-18d729930cb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1643&q=80"
-					title="Sandwich"
-				/>
-				<CardBody>
-					This is the body and it is awesome because everything is so cool!
-					Don't you want to use FrendUI!?
-				</CardBody>
-				<CardActions>
-					<Avatar>+</Avatar>
-					<Avatar>-</Avatar>
-				</CardActions>
-			</Card>
+			<Grid xs={1} sm={2} md={3} lg={5} justifyContent="flex-start" spacing={3}>
+				{testArr.map((val) => (
+					<GridItem>
+						<Card bg="dark">
+							<CardHeader title="Title" subtitle="Subtitle" avatarContent="R" />
+							<CardImage
+								image="https://images.unsplash.com/photo-1614822282964-18d729930cb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1643&q=80"
+								title="Sandwich"
+							/>
+							<CardBody>
+								This is the body and it is awesome because everything is so
+								cool! Don't you want to use FrendUI!?
+							</CardBody>
+							<CardActions>
+								<Avatar>+</Avatar>
+								<Avatar>-</Avatar>
+							</CardActions>
+						</Card>
+					</GridItem>
+				))}
+			</Grid>
 		</div>
 	);
 }
---
+++ src/components/Grid/GridItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./gridItem.css";
+
+const GridItem = ({ children }) => {
+	return <div className="GridItem">{children}</div>;
+};
+
+export default GridItem;
